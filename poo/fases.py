18848fc8468b486eef029830efb91d94226de8c5
landlord245 <gurjant245singh@gmail.com>
2021-12-23 00:05:28 +0100

    main

--- poo/fases.py
+++ poo/fases.py
@@ -6,10 +6,7 @@ class punto:
         print("El puntero es (",self.X,",",self.Y,")")
 
 p1=punto(56,89)
-p2=punto(-45,98)
-p3=punto(78,-5)
-p4=punto(-45,-23)
 p1.mostrarPunto()
-p2.mostrarPunto()
-p3.mostrarPunto()
-p4.mostrarPunto()
+
+p1.X = 7
+p1.mostrarPunto
